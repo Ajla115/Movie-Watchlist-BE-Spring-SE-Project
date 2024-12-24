02ce44ff1fc042e53da324cf5520fa05b7b4d97c
Ajla Korman <ajlakorman@Ajlas-MacBook-Air.local>
2024-12-24 11:37:54 +0100

    Setup

---
+++ src/main/java/ba/edu/ibu/movieswatchlist/MoviesWatchlistApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.movieswatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesWatchlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesWatchlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/edu/ibu/movieswatchlist/MoviesWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.movieswatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesWatchlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
